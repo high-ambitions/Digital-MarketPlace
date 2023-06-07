1a62f3a3a1887a511c25f524a05752f0b440a9c7
AMAN MISHRA <amankumar2021ap@gmail.com>
2023-06-07 17:58:38 +0530

    Payment gateway completed through stripe

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render,get_object_or_404,reverse,redirect
+from .models import Product,OrderDetail
 from django.conf import settings 
 from django.urls import reverse
+from django.http import JsonResponse,HttpResponseNotFound
 #for crosssite req
 from django.views.decorators.csrf import csrf_exempt
 import stripe,json
@@ -19,18 +20,18 @@ def detail(request,id):
 #allows us to make crosssite request with stripe
 @csrf_exempt
 def create_checkout_session(request,id):
-          request_data=json.load(request.body)
-          product=Product.objects.get(id=id)
-          stripe.api_key=settings.STRIPE_SECRET_KEY
-          checkout_session=stripe.checkout.Session.create(
-                customer_email=request_data('email'),
+    request_data=json.loads(request.body)
+    product=Product.objects.get(id=id)
+    stripe.api_key=settings.STRIPE_SECRET_KEY
+    checkout_session=stripe.checkout.Session.create(
+                customer_email=request_data['email'],
                 payment_method_types=['card'],
                 line_items=[
                       {
                             'price_data':{
                                   'currency':'usd',
                                   'product_data':{
-                                        'name':product.name
+                                        'name':product.name,
                                   },
                                   'unit_amount':int(product.price*100)
                             },
@@ -41,8 +42,30 @@ def create_checkout_session(request,id):
                 success_url=request.build_absolute_uri(reverse('success'))+
                 "?session_id={CHECKOUT_SESSION_ID}",
                 cancel_url=request.build_absolute_uri(reverse('failed')),
-          )
-
+    )
           
+    order=OrderDetail()
+    order.customer_email=request_data['email']
+    order.product=product
+    order.stripe_payment_intent=checkout_session['payment_intent']
+    order.amount=int(product.price)
+    order.save()
 
+    return JsonResponse({'sessionId':checkout_session.id})
+
+def payment_success_view(request):
+    session_id=request.GET.get('session_id')
+    if session_id is None:
+         return HttpResponseNotFound()
+    stripe.api_key=settings.STRIPE_SECRET_KEY
+    session=stripe.checkout.Session.retrieve(session_id)
+    order=get_object_or_404(OrderDetail,stripe_payment_intent=session.payment_intent)
+    order.has_paid=True
+    order.save()
+    
+    return render(request,'myapp/payment_success.html',{'order':order})
+
+
+def payment_failed_view(request):
+    return render(request,'myapp/failed.html')
 
