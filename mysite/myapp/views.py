c94c754b0428682d52c9fde51ec2d77a3c1e3529
AMAN MISHRA <amankumar2021ap@gmail.com>
2023-06-09 18:01:28 +0530

    orders page showing purchases done

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -121,3 +121,8 @@ def register(request):
 
 def invalid(request):
     return render(request,'myapp/invalid.html')
+
+def my_purchases(request):
+    orders= OrderDetail.objects.filter(customer_email=request.user.email)
+    return render(request,'myapp/purchases.html',{'orders':orders})
+
